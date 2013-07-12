3c94715cde3d6bf0cef1bab25d302e20087c34f6
akoptsov <sadsorceror@gmail.com>
2013-07-12 12:15:34 +0400

    [JS] improved bubble-sort
    - adds 'sorted' flag for already sorted arrays

--- js/algorithms/bubble.js
+++ js/algorithms/bubble.js
@@ -3,17 +3,24 @@
 
 function sort(array) {
     var last = array.length - 1,
+        sorted,
         i,
         j;
     
     for (i = 0; i <= last; i++) {
-
+        sorted = true;
+        
         for (j = last; j > i; j--) {
 
             if (array[j] < array[j - 1]) {
                 swap(array, j, j - 1);
+                sorted = false;
             }
 
+        }
+        
+        if (sorted) {
+            break;
         }
 
     }
